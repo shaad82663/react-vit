9d9a57b57d99da1b56eb2389de9fee1478391342
Mohd Shadab <mohdshadab@Mohds-MacBook-Air.local>
2025-08-24 21:55:48 +0530

    Added env vars

--- amplify/function/hello/handler.ts
+++ amplify/function/hello/handler.ts
@@ -1,9 +1,12 @@
 import { Schema } from "../../data/resource";
+import {env} from '$amplify/env/sayHello'
 
 type handlerType = Schema["sayHello"]["functionHandler"];
 
 export const handler: handlerType = async (event, context) => {
+  const region = env.REGION;
+  const tableName = env.TABLE_NAME;
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   const {name} = event.arguments || {};
-    return `Hello, ${name || "world"}!`;
+    return `Hello, ${name || "world "}! from ${region} and table ${tableName}`;
 };
--- amplify/function/hello/resource.ts
+++ amplify/function/hello/resource.ts
@@ -1,9 +1,12 @@
-import { defineFunction } from '@aws-amplify/backend'
+import { defineFunction } from "@aws-amplify/backend";
+const REGION = "us-east-1";
+const TABLE_NAME = "MyTable";
 
 export const sayHello = defineFunction({
- name: 'sayHello',
-    entry: 'handler.ts',
-    bundling: {
-        minify: false
-    }
-})
+  name: "sayHello",
+  entry: "handler.ts",
+  bundling: {
+    minify: false,
+  },
+  environment: { REGION, TABLE_NAME },
+});
