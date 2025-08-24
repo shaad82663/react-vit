d5f87800dda9b8296b9f438877fd35ca1063f1b9
Mohd Shadab <mohdshadab@Mohds-MacBook-Air.local>
2025-08-24 21:13:11 +0530

    fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,12 +1,17 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { sayHello } from "../function/hello/resource";
 
+// Add a dummy model (Amplify requires at least one model for data construct)
 const schema = a.schema({
-sayHello: a.query()
-  .arguments({ name: a.string() })
-  .returns(a.string())
-  .handler(a.handler.function(sayHello))
-  .authorization((allow) => [allow.publicApiKey()]),
+  Todo: a.model({
+    content: a.string(),
+  }).authorization((allow) => [allow.publicApiKey()]),
+
+  sayHello: a.query()
+    .arguments({ name: a.string() })
+    .returns(a.string())
+    .handler(a.handler.function(sayHello))
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -14,6 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey"
+    defaultAuthorizationMode: "apiKey",
+    additionalAuthorizationModes: [], // optional, avoids undefined
   },
 });
